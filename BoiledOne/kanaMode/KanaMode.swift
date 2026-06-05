9c202de8568083a7854a29ec4598ac339e9a2926
dagezi <dagezi@gmail.com>
2026-06-04 18:27:48 -0700

    Shrinkable Katakana translation

--- BoiledOne/kanaMode/KanaMode.swift
+++ BoiledOne/kanaMode/KanaMode.swift
@@ -1,15 +1,21 @@
 import Carbon
 class KanaMode : BoiledOneMode {
     let hira: String
+    var result: String
+    var kataChars: Int
 
     init(_ context: BoiledOneContext) {
-        self.hira = romanConverter.convert(source: context.rawString)
+        hira = romanConverter.convert(source: context.rawString)
+        result = hira
+        kataChars = 0
 
         super.init()
 
         commandMap = [
-            CommandEntry(kVK_ANSI_L, [.control], kanaFixCommand),  
+            CommandEntry(kVK_ANSI_K, [.control], kanaKataHiraCommand),
+            CommandEntry(kVK_ANSI_L, [.control], kanaFixCommand),
         ]
+
         for keyCode: Int in BoiledOneMode.selfInsertKeys {
             commandMap.append(CommandEntry(keyCode, [], kanaFixAndExecuteCommand))
         }
@@ -18,8 +24,22 @@ class KanaMode : BoiledOneMode {
 
     override func getShowString(_ context: BoiledOneContext) -> [(Substring, Int)] {
         return [
-            (hira[...], kTSMHiliteSelectedConvertedText),
+            (result[...], kTSMHiliteSelectedConvertedText),
         ]
+    }
+
+    func shrinkKatakana() {
+        if kataChars > 0 {
+            kataChars -= 1
+        } else {
+            kataChars = hira.count
+        }
+
+        if (kataChars == 0) {
+            result = hira
+        } else {
+            result = hiraToKata(hira, range: hira.startIndex ..< hira.index(hira.startIndex, offsetBy: kataChars))
+        }
     }
 }
 
@@ -31,13 +51,24 @@ let kanaStartCommand = BoiledOneCommand(
     }
 )
 
-let kanaFixCommand = BoiledOneCommand(
-    name: "KanaFixCommand", 
+let kanaKataHiraCommand = BoiledOneCommand(
+    name: "KanaKataHiraCommand",
     handler: {context in
-        guard let mode = context.mode as? KanaMode else {
+        guard let mode: KanaMode = context.mode as? KanaMode else {
             return .handled
         }
-        context.insertToClient(mode.hira)
+        mode.shrinkKatakana()
+        return .handled
+    }
+)
+
+let kanaFixCommand = BoiledOneCommand(
+    name: "KanaFixCommand",
+    handler: {context in
+        guard let mode: KanaMode = context.mode as? KanaMode else {
+            return .handled
+        }
+        context.insertToClient(mode.result)
         context.mode = NoneMode()
         context.rawString = ""
         return .handled
